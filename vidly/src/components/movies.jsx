4e13373b97f6097cee7fb57e7e8edf058731c4fd
Trong Hieu DalatCoder <hieuntctk42@gmail.com>
2020-09-03 17:39:44 +0700

    display all movies

--- vidly/src/components/movies.jsx
+++ vidly/src/components/movies.jsx
@@ -17,7 +17,9 @@ class Movies extends Component {
   };
 
   componentDidMount() {
-    this.setState({ movies: getMovies(), genres: getGenres() });
+    const genres = [{ name: 'All Genres' }, ...getGenres()];
+
+    this.setState({ movies: getMovies(), genres });
   }
 
   handleLike = ({ _id }) => {
@@ -39,7 +41,7 @@ class Movies extends Component {
   };
 
   handleGenreSelect = (genre) => {
-    this.setState({ selectedGenre: genre });
+    this.setState({ selectedGenre: genre, currentPage: 1 });
   };
 
   render() {
@@ -58,9 +60,10 @@ class Movies extends Component {
         </p>
       );
 
-    const filtered = selectedGenre
-      ? allMovies.filter((m) => m.genre._id === selectedGenre._id)
-      : allMovies;
+    const filtered =
+      selectedGenre && selectedGenre._id
+        ? allMovies.filter((m) => m.genre._id === selectedGenre._id)
+        : allMovies;
 
     const movies = paginate(filtered, currentPage, pageSize);
 
