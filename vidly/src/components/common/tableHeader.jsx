62b9e219edff4068af58b1e12c894a7365859202
Trong Hieu DalatCoder <hieuntctk42@gmail.com>
2020-09-06 10:00:41 +0700

    add sorting icon

--- vidly/src/components/common/tableHeader.jsx
+++ vidly/src/components/common/tableHeader.jsx
@@ -13,16 +13,24 @@ class TableHeader extends Component {
     this.props.onSort(sortColumn);
   };
 
+  renderSortIcon = (column) => {
+    if (column.path !== this.props.sortColumn.path) return null;
+    if (this.props.sortColumn.order === 'asc')
+      return <i className="fa fa-sort-asc"></i>;
+    return <i className="fa fa-sort-desc"></i>;
+  };
+
   render() {
     return (
       <thead>
         <tr>
           {this.props.columns.map((column) => (
             <th
+              className="clickable"
               key={column.key || column.path}
               onClick={() => this.raiseSort(column.path)}
             >
-              {column.label}
+              {column.label} {this.renderSortIcon(column)}
             </th>
           ))}
         </tr>
