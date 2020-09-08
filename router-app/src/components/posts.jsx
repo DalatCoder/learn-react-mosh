4f61a620f65a73b533d8a8b4f784d0a38305d120
Trong Hieu DalatCoder <hieuntctk42@gmail.com>
2020-09-08 21:02:44 +0700

    use query-string to parse params from query in URL

--- router-app/src/components/posts.jsx
+++ router-app/src/components/posts.jsx
@@ -1,6 +1,10 @@
 import React from 'react';
+import queryString from 'query-string';
 
-const Posts = ({ match: { params } }) => {
+const Posts = ({ match: { params }, location: { search } }) => {
+  const result = queryString.parse(search);
+  console.log(result);
+
   return (
     <div>
       <h1>Posts</h1>
